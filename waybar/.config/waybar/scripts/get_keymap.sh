61cc0b4dc957aa37201007679866db871043abdc
humberto carmona <carmona@fisica.ufc.br>
2023-05-10 16:28:42 -0300

    fix waybar get_keymap

--- waybar/.config/waybar/scripts/get_keymap.sh
+++ waybar/.config/waybar/scripts/get_keymap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-kmap=$(swaymsg -t get_inputs | jq '.[] | select(.type=="keyboard") | select((.name|contains("keyboard")) or (.name|contains("Keychron"))) | select(.libinput.accel_speed==null) | .xkb_active_layout_name')
+kmap=$(swaymsg -t get_inputs | jq '.[] | select(.type=="keyboard") | select(.name|contains("Keychron")) | select(.libinput.accel_speed==null) | .xkb_active_layout_name')
+if [ -z "$kmap" ]; then
+	kmap=$(swaymsg -t get_inputs | jq '.[] | select(.type=="keyboard") | select(.name|contains("keyboard")) | select(.libinput.accel_speed==null) | .xkb_active_layout_name')
+fi
 
 case "$kmap" in
 "\"English (US)\"")
