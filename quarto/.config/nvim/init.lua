c15d94eddbe595e323d6163b220cdfd0c515e6f4
humberto carmona <carmona@fisica.ufc.br>
2023-05-22 17:19:00 -0300

    vscodium settings

--- quarto/.config/nvim/init.lua
+++ quarto/.config/nvim/init.lua
@@ -1,4 +1,12 @@
-require("config.global")
-require("config.lazy")
-require("config.keymap")
-require("config.autocommands")
+if vim.g.vscode then
+    require("config.global")
+    require("config.lazy")
+    require("config.keymap")
+    require("config.autocommands")
+    require("notify")("here vscode")
+else
+    require("config.global")
+    require("config.lazy")
+    require("config.keymap")
+    require("config.autocommands")
+end
