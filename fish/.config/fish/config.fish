b6d477e78d0ce85ba6a8e48b84140e9e654e28e2
humberto carmona <carmona@fisica.ufc.br>
2023-06-29 17:10:32 -0300

    small

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,7 @@ set -gx GOPATH $HOME/.go
 set -gx BAT_THEME Catppuccin-mocha
 # set -gx FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore .cache -g ""'
 set -gx FZF_DEFAULT_OPTS "--layout=reverse --height 50% --preview='bat --color=always {}'"
-
+set -gx TEXMFHOME $HOME/.texmf
 set -U fish_user_paths $GOPATH/bin $fish_user_paths
 set -U fish_user_paths $HOME/.config/bin $fish_user_paths
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
