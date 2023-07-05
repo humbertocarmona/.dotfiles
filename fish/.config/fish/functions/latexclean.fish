42d3f58aaf8c823ae9a0a8ac5cca2330f826bf57
Humberto Carmona <carmona@fisica.ufc.br>
2023-07-05 18:37:00 -0300

    latexclean

---
+++ fish/.config/fish/functions/latexclean.fish
@@ -0,0 +1,11 @@
+function latexclean 
+  rm -f *.aux 
+  rm -f *.bbl 
+  rm -f *.bcf 
+  rm -f *.fdb_latexmk 
+  rm -f *.fls 
+  rm -f *.log 
+  rm -f *.run.xml 
+  rm -f *.synctex.gz 
+  rm -f *.toc       
+end
