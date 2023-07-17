1ea1ca450e0bb9e392f558c62681142d40a9a49f
humberto carmona <carmona@fisica.ufc.br>
2023-07-17 17:01:53 -0300

    swayimg

--- LazyStarter/.config/nvim/lua/plugins/coding.lua
+++ LazyStarter/.config/nvim/lua/plugins/coding.lua
@@ -4,15 +4,19 @@ return {
         -- ft = { "julia", "python" },
         dependencies = { "jpalardy/vim-slime" },
         keys = {
-            { "<leader>dc", "<Plug>SlimeConfig", desc = "Slime config" },
-            { "<leader>ds", "<Plug>SlimeCellsSendAndGoToNext", desc = "Send and go to next" },
-            { "<leader>dj", "<Plug>SlimeCellsNext", desc = "Go to next" },
-            { "<leader>dk", "<Plug>SlimeCellsPrev", desc = "Go to prev" },
+            { "<leader>cc", "<Plug>SlimeConfig", desc = "Slime config" },
+            { "<leader>cs", "<Plug>SlimeCellsSendAndGoToNext", desc = "Send and go to next" },
+            { "<leader>cj", "<Plug>SlimeCellsNext", desc = "Go to next" },
+            { "<leader>ck", "<Plug>SlimeCellsPrev", desc = "Go to prev" },
         },
         config = function()
-            vim.g.slime_target = "kitty"
-            vim.g.slime_default_config = { kitty_listen_on = "2" }
-
+            if os.getenv("TMUX") then
+                vim.g.slime_target = "tmux"
+                vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
+            else
+                vim.g.slime_target = "kitty"
+                vim.g.slime_default_config = { kitty_listen_on = "2" }
+            end
             vim.g.slime_cell_delimiter = "^\\s*##"
             vim.g.slime_dont_ask_default = 1
             vim.g.slime_bracketed_paste = 1
@@ -20,5 +24,3 @@ return {
         end,
     },
 }
--- vim.g.slime_target = "tmux"
--- vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
