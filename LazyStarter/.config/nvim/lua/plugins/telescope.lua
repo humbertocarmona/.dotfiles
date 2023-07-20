954a02a878a689b689d16fb9ab6145887601bbba
Humberto Carmona <carmona@fisica.ufc.br>
2023-07-20 14:08:49 -0300

    Telescope read templates

---
+++ LazyStarter/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            {
+                "<leader>tR",
+                ":read ~/.config/nvim/templates/Subfiles/Root.tex<cr>",
+                desc = "Root.tex",
+            },
+            {
+                "<leader>tS",
+                ":read ~/.config/nvim/templates/Subfiles/Subfile.tex<cr>",
+                desc = "Subfile.tex",
+            },
+            {
+                "<leader>tA",
+                ":read ~/.config/nvim/templates/Subfiles/A-Includes.tex<cr>",
+                desc = "A-Includes.tex",
+            },
+            {
+                "<leader>tB",
+                ":read ~/.config/nvim/templates/Subfiles/B-Notation.tex<cr>",
+                desc = "B-Notation.tex",
+            },
+            {
+                "<leader>tC",
+                ":read ~/.config/nvim/templates/Subfiles/C-References.bib<cr>",
+                desc = "C-References.bib",
+            },
+        },
+    },
+}
