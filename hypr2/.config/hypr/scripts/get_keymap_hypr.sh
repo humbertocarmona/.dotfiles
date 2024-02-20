41d593fde7ae8a26362ffcb762ceb1b3e0319e52
Humberto <carmona@fisica.ufc.br>
2024-02-20 14:41:44 -0300

    bug keycron

--- hypr2/.config/hypr/scripts/get_keymap_hypr.sh
+++ hypr2/.config/hypr/scripts/get_keymap_hypr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kmap=$(hyprctl -j devices | jq '.keyboards | .[] |select(.name == "keychron-keychron-k2") | .active_keymap')
+
 if [ -z "$kmap" -a "$kmap" != " " ]; then
 	kmap=$(hyprctl -j devices | jq '.keyboards | .[] |select(.name|contains("logitech-wireless-keyboard")) | .active_keymap')
 fi
