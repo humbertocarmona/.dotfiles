dc9852776a31fa97a5ce25db88386188a1b19bd3
Humberto de Andrade Carmona <carmona@fisica.ufc.br>
2024-03-02 14:28:23 -0300

    hypr move window to next workspace

---
+++ hypr2/.config/hypr/scripts/move_next_workspace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo $0
+echo $1
+
+zero=0
+# get active workspace
+w="$(hyprctl activeworkspace -j | jq '.id')"
+
+if [ $1 -eq $zero ]; then
+	((w += 1))
+else
+	((w -= 1))
+fi
+
+if [ $w -gt $zero ]; then
+	hyprctl dispatch movetoworkspace $w
+fi
