d4b9b9c580d668ad11e332287dab9a3f98a6a1f7
Humberto Carmona <carmona@fisica.ufc.br>
2024-04-22 12:30:20 -0300

    fix next workspace

--- hypr2/.config/hypr/scripts/next_workspace.sh
+++ hypr2/.config/hypr/scripts/next_workspace.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo $0
-echo $1
+echo $0 $1
 
 zero=0
 # get active workspace
@@ -10,15 +9,17 @@ w="$(hyprctl workspaces -j | jq .[].id)"
 # go back
 
 if [ $1 -gt $zero ]; then
-	for n in $(hyprctl workspaces -j | jq .[].id | tac); do
-		if [[ ($n > 0) && ($n -lt $c) ]]; then
+	for n in $(hyprctl workspaces -j | jq 'sort_by(.id) | .[].id ' | tac); do
+		echo $c $n
+		if [[ ($n -gt $zero) && ($n -lt $c) ]]; then
 			hyprctl dispatch workspace $n
+			echo "vou break"
 			break
 		fi
 	done
 else
-	for n in $(hyprctl workspaces -j | jq .[].id); do
-		if [[ $n -gt $c ]]; then
+	for n in $(hyprctl workspaces -j | jq 'sort_by(.id) | .[].id'); do
+		if [[ ($n -gt $zero) && ($n -gt $c) ]]; then
 			hyprctl dispatch workspace $n
 			break
 		fi
