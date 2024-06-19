9526854f613de6efbdbd7a0ca82ebbab5128587e
Humberto Carmona <carmona@fisica.ufc.br>
2024-06-19 15:27:02 -0300

    Braille font

--- fish/.config/fish/functions/dpat.fish
+++ fish/.config/fish/functions/dpat.fish
@@ -4,6 +4,6 @@ function dpat
     if test (count $argv) -lt 1
         set target $HOME
     end
-    set -l res (fd -E .pyenv -E .cache -E Trash -E .virtualenvs -H -I -i -L -t d . $target | fzf --height 50% --layout reverse --preview='exa --tree -L 2 {}')
+    set -l res (fd -E .pyenv -E .cache -E Trash -E .virtualenvs -H -I -i -L -t d . $target | fzf --height 50% --layout reverse --preview='eza --tree -L 2 {}')
     cd $res
 end
