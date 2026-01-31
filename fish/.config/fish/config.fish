cc6b23ee93cb0fe24def6e57a220a24efdc277d6
Humberto Carmona <carmona@fisica.ufc.br>
2026-01-31 15:51:10 -0300

    auto tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,22 +10,14 @@ set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set -U fish_user_paths $HOME/go/bin $fish_user_paths
 set -U fish_user_paths $HOME/.juliaup/bin $fish_user_paths
-#fish_add_path $HOME/.pyenv/bin
-
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
-# fish_add_path $PYENV_ROOT/bin
-# set -gx PYENV_ROOT $HOME/.pyenv
-# set -gx PATH $PYENV_ROOT/bin $PATH
-# status --is-interactive; and pyenv init - | source
-# status --is-interactive; and pyenv virtualenv-init - | source
 
 fish_vi_key_bindings
 fish_vi_cursor
 
 source $HOME/.config/fish/fish_aliases
 zoxide init fish | source
-# set -x XDG_CONFIG_HOME $HOME/.config
 
 #pyenv
 set -Ux PYENV_ROOT $HOME/.pyenv
@@ -33,3 +25,15 @@ fish_add_path $PYENV_ROOT/bin
 
 status --is-interactive; and pyenv init --path | source
 status --is-interactive; and pyenv init - | source
+
+#tmux
+# Auto-start tmux on interactive shells
+if status is-interactive
+    # Don't start tmux inside tmux
+    if not set -q TMUX
+        # Optional: only on local terminals
+        if test -z "$SSH_CONNECTION"
+            tmux attach-session -t default || tmux new-session -s default
+        end
+    end
+end
